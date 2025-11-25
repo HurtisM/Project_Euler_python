c1d2a285499ec27e4f0f0067a4d25c6df7758327
Lexa <martin.hurtis@outlook.sk>
2025-11-25 20:55:36 +0100

    problem 76 solution

---
+++ PE_solutions/P076.py
@@ -0,0 +1,12 @@
+# Problem 76
+def CountWays(n):
+    table = [0] * (n + 1)
+    table[0] = 1
+    for i in range(1, n):
+        for j in range(i, n + 1):
+            table[j] += table[j - i]
+    return table[n]
+
+if __name__ == '__main__':
+    limit = 100
+    print(CountWays(limit))
